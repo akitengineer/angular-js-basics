413abf72299e9e2bde3c8c7fc531e27ce6745177
ak.itengineer <ak.itengineer@yahoo.com>
2015-02-24 22:12:00 +0530

    Commit for testing

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,8 +1,16 @@
 (function () {
 	var app = angular.module('myApp', ['ngRoute']);
 	
+	
+	/*
+	 * Config use to set routes.
+	 */
+	
+	
 	app.config(['$routeProvider', function ($routeProvider) {
-			$routeProvider.when('/topic1',{
+			$routeProvider.when('/topic0',{
+				templateUrl: 'views/topic0.html',
+			}).when('/topic1',{
 				templateUrl: 'views/topic1.html',
 			}).when('/topic2',{
 				templateUrl: 'views/topic2.html',
@@ -14,51 +22,89 @@
 				templateUrl: 'views/topic5.html',
 			}).when('/topic6',{
 				templateUrl: 'views/topic6.html',
+			}).when('/goodbye',{
+				templateUrl: 'views/goodbye.html',
 			}).otherwise({
 				redirectTo:'/'
 			});
 	}]);
 
+	
+	/*
+	 * Main controller
+	 */
 	app.controller('topicsController', ['$scope', function ($scope) {
 			$scope.topics = topics;
 	}]);
 
 	var topics = [
+		
 		{
-			name : 'Topic 1',
+			name : 'Introduction',
+			link : '#/topic0'
+		},
+		{
+			name : 'Topic 1: Module, Directive, Two Data Binding',
 			link : '#/topic1'
 		},
 		{
-			name : 'Topic 2',
+			name : 'Topic 2: Controllers',
 			link : '#/topic2'
 			
 		},
 		{
-			name : 'Topic 3',
+			name : 'Topic 3: $scope, Dependency Injection, ng-repeat directive, filters, expression',
 			link : '#/topic3'
 		},
 		{
-			name : 'Topic 4',
+			name : 'Topic 4: Services in AngularJS, predefined, custom',
 			link : '#/topic4'
 		},
 		{
-			name : 'Topic 5',
+			name : 'Topic 5: Custom Directives',
 			link : '#/topic5'
 		},
 		{
-			name : 'Topic 6',
+			name : 'Topic 6: Advantages & Disadvantages',
 			link : '#/topic6'
 		},
 	];
+	
+	
+	
+	/*
+	 * Topic 1 controller
+	 */
+	app.controller('topic1Controller', ['$scope', function ($scope) {
+		$scope.myName = "Newbie";	
+		$scope.needExplanation = false;
+		$scope.showImages = function () {
+			$scope.needExplanation = true;
+		}
+	}]);
+
+
+	/*
+	 * Topic 2 controller
+	 */
 	app.controller("topic2Controller", function () {
 	   this.msg = "Hi! incaendo Team i am angularJS";
 	
    });
 	 
+	 
+	 /*
+	  * topic 3 controller
+	  */
 	 app.controller('topic3Controller',["$scope", function ($scope) {
 		 $scope.msg = "Hello again incaendo team!. Lets learn something new. :)";
 		 $scope.employees = employees;
 	 }]);
+ 
+ 
+	 /*
+	  * Topic 4 controller
+	  */
  
    app.controller('topic4Controller', ["$scope","$http", "topic4Service", function ($scope, $http, topic4Service) {
 			 $http.get("http://graph.facebook.com/youtube")
@@ -66,16 +112,24 @@
 			 $scope.msg = topic4Service;
 	 }]);
  
+ 
+	 /*
+	  * Topic 4 services
+	  */
 	 app.factory('topic4Service', ["$http", function ($http) {
 			 return "Hello";
 			 		 
 	 }]);
 	 
+	 /*
+	  * topic 5 controller
+	  */
 	 app.directive('myDetails', function () {
 		 return {
 			 template: 'Name: {{employee.name}} Desgination:{{employee.designation}}, experience:{{employee.address}}'
 		 }
 	 });
+	
 	 
 	 var employees = [
 		 {
